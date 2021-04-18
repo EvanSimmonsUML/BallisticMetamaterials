1b3c240e561360ae652c0174fe4ad27a5efdf8b5
EvanSimmonsUML <82781000+EvanSimmonsUML@users.noreply.github.com>
2021-04-18 17:20:47 -0400

    Add files via upload

---
+++ README.m
@@ -0,0 +1,17 @@
+%EXPLANATION OF SCRIPTS%
+
+%% 1. epsMainScript.m 
+% EXPLANATION: Provides the user with the calculation of the effective 
+% permittivity for the composite discussed in "Ballistic Metamaterials"
+% (https://doi.org/10.1364/OPTICA.402891). Specifically, when run, the code
+% provides the in-plane and perpendicular components of the composite dielectric
+% tensor for HMM3 (4um composite with individual InGaAs/AlInAs of 9.5nm).
+% Run this script to illustrate the dielectric tensor components used in
+% R/T/A calculations. 
+
+%% 2. tmmMainScript.m
+% EXPLANATION: Provides user with absorption spectrum of HMM3 from the
+% manuscript (see above). The resulting plot displays the absorption as a
+% surface plot as a function of the incident angle (y-axis) and wavelength 
+%(x-axis). The script also saves the reflection coefficient and
+%transmission coefficient. 
